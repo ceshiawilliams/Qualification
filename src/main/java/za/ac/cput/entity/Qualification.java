ae0a9c59fdbcf556eb0b628de1b9c6b2877e63da
ceshia <ceshiaw21@gmail.com>
2020-06-23 16:40:43 +0200

    Updated Qualification

--- src/main/java/za/ac/cput/entity/Qualification.java
+++ src/main/java/za/ac/cput/entity/Qualification.java
@@ -10,8 +10,46 @@ public class Qualification {
 
     public static void main( String[] args )
     {
-        //needs to be edited
+
     }
 
+    private Qualification(String numberOfSubjects, String levelOfQualifications) {
+        this.numberOfSubjects = numberOfSubjects;
+        this.levelOfQualifications = levelOfQualifications;
+    }
 
+    public String getNumberOfSubjects() {
+        return numberOfSubjects;
+    }
+
+    public String getLevelOfQualifications() {
+        return levelOfQualifications;
+    }
+
+    @Override
+    public String toString() {
+        return "Qualification{" +
+                "numberOfSubjects='" + numberOfSubjects + '\'' +
+                ", levelOfQualifications='" + levelOfQualifications + '\'' +
+                '}';
+    }
+
+    public static class QualificationBuilder {
+        private String numberOfSubjects;
+        private String levelOfQualifications;
+
+        public QualificationBuilder setNumberOfSubjects(String numberOfSubjects) {
+            this.numberOfSubjects = numberOfSubjects;
+            return this;
+        }
+
+        public QualificationBuilder setLevelOfQualifications(String levelOfQualifications) {
+            this.levelOfQualifications = levelOfQualifications;
+            return this;
+        }
+
+        public Qualification createQualification() {
+            return new Qualification(numberOfSubjects, levelOfQualifications);
+        }
+    }
 }
